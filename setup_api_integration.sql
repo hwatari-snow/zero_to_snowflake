b02a8b620dff5e8a92bfa8075f6cbd7b671e51eb
hiroki watari <hiroki.watari@snowflake.com>
2025-11-16 14:01:21 +0000

    nice

--- setup_api_integration.sql
+++ setup_api_integration.sql
@@ -5,7 +5,7 @@ use schema snowflake_learning_db.public; -- ここは自分のスキーマに変
 -- Git統合用のAPI統合
 CREATE OR REPLACE API INTEGRATION handson_github_api_integration
    API_PROVIDER = git_https_api
-   API_ALLOWED_PREFIXES = ('https://github.com/sfc-gh-tshoji/zero-to-snowflake')
+   API_ALLOWED_PREFIXES = ('https://github.com/')
    API_USER_AUTHENTICATION = (
       TYPE = snowflake_github_app
    )
